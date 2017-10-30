5dd14960c753dc8f4797df5a45c17f9bc3373a8b
Fahrenheit <fahrenheitindi@gmail.com>
2017-10-30 17:57:19 +0300

    add slider, parts item and some fix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,12 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import Siema from 'siema';
 
 $(() => {
 	svg4everybody();
+});
+
+new  Siema({
+	selector: '.banner',
+	loop: true
 });

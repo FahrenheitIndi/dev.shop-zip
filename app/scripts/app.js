b0453db70d601704cec70a9dbf0a84a30ed8c18e
Fahrenheit <fahrenheitindi@gmail.com>
2017-10-31 16:53:11 +0300

    add catalog item and qustion form

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,18 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 import Siema from 'siema';
+import Inputmask from 'inputmask';
+
+
+new Inputmask({mask: '+7 (999) 999-99-99'}).mask('input[type=tel]');
+
+new Siema({selector: '.banner', loop: true});
+
+new Siema({selector: '.article', loop: true, perPage: 4});
 
 $(() => {
 	svg4everybody();
 });
 
-new  Siema({
-	selector: '.banner',
-	loop: true
-});
+
+

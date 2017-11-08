af5e4f53a26e5ccd4bd5d8b076f5a733fbe68ab8
Fahrenheit <fahrenheitindi@gmail.com>
2017-11-08 21:28:17 +0300

    redisine header and change color

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,16 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
-import Siema from 'siema';
+import Banner from 'siema';
+import Carousel from 'siema';
 import Inputmask from 'inputmask';
-
-
 new Inputmask({mask: '+7 (999) 999-99-99'}).mask('input[type=tel]');
-
-new Siema({selector: '.banner', loop: true});
-
-new Siema({selector: '.article', loop: true, perPage: 4});
+new Banner({selector: '.banner', loop: true});
+const carousel = new Carousel({selector: '.article', loop: true, perPage: 4});
+document.querySelector('.articles__button_left').addEventListener('click', () => carousel.prev());
+document.querySelector('.articles__button_right').addEventListener('click', () => carousel.next());
+document.querySelector('.menu__item_sub').addEventListener('hover', () => document.querySelector('.main').classList.add('.main_hover'));
 
 $(() => {
 	svg4everybody();
 });
-
-
 

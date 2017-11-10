5c636929bd38a1cf7de17f46701ec7ca3b6cb6a5
Fahrenheit <fahrenheitindi@gmail.com>
2017-11-10 12:22:34 +0300

    end change

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,9 +8,7 @@ new Banner({selector: '.banner', loop: true});
 const carousel = new Carousel({selector: '.article', loop: true, perPage: 4});
 document.querySelector('.articles__button_left').addEventListener('click', () => carousel.prev());
 document.querySelector('.articles__button_right').addEventListener('click', () => carousel.next());
-document.querySelector('.menu__item_sub').addEventListener('hover', () => document.querySelector('.main').classList.add('.main_hover'));
-
 $(() => {
 	svg4everybody();
+	//$('.menu__item_sub').hover(() => {$('.main').toggleClass('main-hover');});
 });
-

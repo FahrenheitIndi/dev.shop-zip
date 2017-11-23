27126a7ea2cfc14c83d07e2de0ba8b4d19d67bd0
Fahrenheit <fahrenheitindi@gmail.com>
2017-11-23 17:29:44 +0300

    add service page and fix bugs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,12 +3,12 @@ import $ from 'jquery';
 import Banner from 'siema';
 import Carousel from 'siema';
 import Inputmask from 'inputmask';
-new Inputmask({mask: '+7 (999) 999-99-99'}).mask('input[type=tel]');
+new Inputmask({mask: '+7 (999) 999-99-99'}).mask('#phone, .field-group__input_phone');
 new Banner({selector: '.banner', loop: true});
 const carousel = new Carousel({selector: '.article', loop: true, perPage: 4});
 document.querySelector('.articles__button_left').addEventListener('click', () => carousel.prev());
 document.querySelector('.articles__button_right').addEventListener('click', () => carousel.next());
 $(() => {
 	svg4everybody();
-	//$('.menu__item_sub').hover(() => {$('.main').toggleClass('main-hover');});
+	$('.menu__item_sub').hover(() => {$('.cover').toggleClass('cover-hover');});
 });
